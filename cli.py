a4152ac626fde970984182a45559cc12d70c9fd1
MuhammedYahiya <yahiyahmhd@gmail.com>
2025-04-28 18:06:02 +0530

    feat: add MySQL backup functionality

--- cli.py
+++ cli.py
@@ -1,5 +1,5 @@
 import click
-from db_connector import connect_mysql
+from db_connector import connect_mysql, backup_mysql
 
 @click.group()
 def cli():
@@ -19,7 +19,28 @@ def test_mysql_connection(host, port, user, password, database):
         click.echo("Connection successful")
     else:
         click.echo("Connection failed")
+        
+        
+@cli.command()
+@click.option('--host', prompt='Database Host', help='Host of the database')
+@click.option('--port', prompt='Database Port', default=3306, help='Port of the database')
+@click.option('--user', prompt='Username', help='Username for the database')
+@click.option('--password', prompt='Password', help='Password for the database')
+@click.option('--database', prompt='Database Name', help='Name of the database to connect')
+@click.option('--output-file', prompt='Backup File Name (without extension)', help='Name of the output backup file')
+def backup_mysql_command(host, port, user, password, database, output_file):
+    """Backup MySQL database to a .sql file."""
+    connection = connect_mysql(host, port, user, password, database)
 
+    if connection:
+        success = backup_mysql(host, port, user, password, database, output_file)
+        if success:
+            click.echo(f"Backup saved successfully as {output_file}.sql")
+        else:
+            click.echo(f"Backup failed")
+    else:
+        click.echo("Could not connect to the database. Backup not performed")
+        
 @cli.command()
 def backup():
     """Perform a backup"""
--- db_connector.py
+++ db_connector.py
@@ -1,4 +1,5 @@
 import pymysql
+import subprocess
 
 def connect_mysql(host, port, user, password, database):
     try:
@@ -14,3 +15,28 @@ def connect_mysql(host, port, user, password, database):
     except pymysql.MySQLError as e:
         print(f"MySQL connection Failed: {e}")
         return None
+    
+    
+def backup_mysql(host, port, user, password, database, output_file):
+    """Backup MysQL database and save to a .sql file."""
+    try:
+        command = [
+            'mysqldump',
+            '-h',host,
+            '-P', str(port),
+            '-u', user,
+            '-p' + password,
+            database,
+        ]
+        
+        output_file_path = f"{output_file}.sql"
+        
+        with open(output_file_path, 'w') as backup_file:
+            subprocess.run(command, stdout=backup_file, stderr=subprocess.PIPE, check=True)
+            
+        print(f"Backup successful! saved to {output_file_path}")
+        return True
+    
+    except subprocess.CalledProcessError as e:
+        print(f"Backup failed: {e}")
+        return False
