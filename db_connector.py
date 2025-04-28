2a8ae09723c252e4671d95533926e4b2500fa406
MuhammedYahiya <yahiyahmhd@gmail.com>
2025-04-28 18:42:31 +0530

    feat: Add compression and directory management for backups

--- db_connector.py
+++ db_connector.py
@@ -1,5 +1,7 @@
 import pymysql
 import subprocess
+import os
+import zipfile
 
 def connect_mysql(host, port, user, password, database):
     try:
@@ -17,8 +19,12 @@ def connect_mysql(host, port, user, password, database):
         return None
     
     
-def backup_mysql(host, port, user, password, database, output_file):
+def backup_mysql(host, port, user, password, database, output_file, backup_dir='backups'):
     """Backup MysQL database and save to a .sql file."""
+    
+    if not os.path.exists(backup_dir):
+        os.makedirs(backup_dir)
+        print(f"Backup directory '{backup_dir}' created.")
     try:
         command = [
             'mysqldump',
@@ -29,12 +35,18 @@ def backup_mysql(host, port, user, password, database, output_file):
             database,
         ]
         
-        output_file_path = f"{output_file}.sql"
+        output_sql_path = os.path.join(backup_dir,f"{output_file}.sql")
+        compressed_backup_path = os.path.join(backup_dir,f"{output_file}.zip")
         
-        with open(output_file_path, 'w') as backup_file:
+        with open(output_sql_path, 'w') as backup_file:
             subprocess.run(command, stdout=backup_file, stderr=subprocess.PIPE, check=True)
             
-        print(f"Backup successful! saved to {output_file_path}")
+        with zipfile.ZipFile(compressed_backup_path, 'w', zipfile.ZIP_DEFLATED) as zipf:
+            zipf.write(output_sql_path, os.path.basename(output_sql_path))
+            
+        os.remove(output_sql_path)
+        
+        print(f"Backup successful! saved to {output_sql_path}")
         return True
     
     except subprocess.CalledProcessError as e:
